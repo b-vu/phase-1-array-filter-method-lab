46469ddacb1c86ca3885e01d32081d99145b1b47
Brian Vu <brianvu7@gmail.com>
2022-02-03 10:25:00 -0600

    commit tree 9b229aa2f77f24e886b1238d9abb838e1efc08a8

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+const findMatching = (array, name) => {
+    const lowercaseName = name.toLowerCase();
+    const newArray = array.filter(item => item === name || item.toLowerCase() === lowercaseName);
+    return newArray;
+}
+
+const fuzzyMatch = (array, name) => {
+    return array.filter(item => item.substring(0, name.length) === name);
+}
+
+const matchName = (array, name) => {
+    return array.filter(item => item.name === name);
+}
